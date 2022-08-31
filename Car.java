7beff83b977641fac891cc60c1e9f44ceee073f1
fengqijie588 <fengqijie588@gmail.com>
2022-08-31 22:05:22 +0900

    no message

---
+++ Car.java
@@ -0,0 +1,15 @@
+public class Car{
+	String color;
+	public Car() {
+		System.out.println("车是红色的");
+	}
+	public static void main(String[]args) {
+		System.out.println("车很大");
+		Car lemonadStand = new Car();
+	    
+	    System.out.println(lemonadStand );
+	}
+		
+
+	
+}
