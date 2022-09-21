b880ba5318f7f45e26766cc8908e24d6ec116dbd
fengqijie588 <fengqijie588@gmail.com>
2022-09-21 22:43:04 +0900

    循环

---
+++ While.java
@@ -0,0 +1,22 @@
+public class While{
+	public static void main(String[]arge) {
+		/*int X = 1;
+		do {
+			System.out.println("X的值:"+X);
+			X++;
+			System.out.println("\n");
+			
+		}
+		while (X<30);
+		
+		for(int i=1;i<=9;i++){
+		for(int j=1;j<=i;j++){
+		System.out.print(j+"*"+i+"="+i*j+" ");
+		}
+		System.out.println();*/
+		for(int X=1; X<10;X=X+1) {
+			System.out.println("X的值:"+X);
+	
+		}
+		}
+	}
